5b28df52c0cdb03f11e1a6ad20c826b4fe050c93
河野菖太 <peprium@kounonoMacBook-Air.local>
2024-11-20 23:32:37 +0900

    Create: User-model controller view

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,13 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
+
+exec "$@"
+
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
