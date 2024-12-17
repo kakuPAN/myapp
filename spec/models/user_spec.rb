384beb24a2fafd5182e56f4d3adcea54ca434eae
河野菖太 <peprium@kounonoMacBook-Air.local>
2024-12-17 19:52:48 +0900

    test: Adding RSpec test such as user model spec, task model spec, task system spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,28 +10,28 @@ RSpec.describe User, type: :model do
     it 'user_nameがない場合にバリデーションが機能してinvalidになるか' do
       user_without_user_name = build(:user, user_name: "")
       expect(user_without_user_name).to be_invalid
-      expect(user_without_user_name.errors[ :user_name ]).to eq ["を入力してください"]
+      expect(user_without_user_name.errors[:user_name]).to eq ["を入力してください"]
     end
     it 'emailがない場合にバリデーションが機能してinvalidになるか' do
       user_without_email = build(:user, email: "")
       expect(user_without_email).to be_invalid
-      expect(user_without_email.errors[ :email ]).to eq ["を入力してください"]
+      expect(user_without_email.errors[:email]).to eq ["を入力してください"]
     end
     it 'passwordがない場合にバリデーションが機能してinvalidになるか' do
       user_without_password = build(:user, password: "")
       expect(user_without_password).to be_invalid
-      expect(user_without_password.errors[ :password ]).to eq ["は3文字以上で入力してください"]
+      expect(user_without_password.errors[:password]).to eq ["は3文字以上で入力してください"]
     end
     it 'password_confirmationがない場合にバリデーションが機能してinvalidになるか' do
       user_without_password_confirmation = build(:user, password_confirmation: "")
       expect(user_without_password_confirmation).to be_invalid
-      expect(user_without_password_confirmation.errors[ :password_confirmation ]).to eq ["とパスワードの入力が一致しません", "を入力してください"]
+      expect(user_without_password_confirmation.errors[:password_confirmation]).to eq ["とパスワードの入力が一致しません", "を入力してください"]
     end
     it 'emailが被った場合にuniqueのバリデーションが機能してinvalidになるか' do
       user = create(:user)
       user_with_duplicated_email= build(:user, email: user.email)
       expect(user_with_duplicated_email).to be_invalid
-      expect(user_with_duplicated_email.errors[ :email ]).to eq ["はすでに存在します"]
+      expect(user_with_duplicated_email.errors[:email]).to eq ["はすでに存在します"]
     end
     it 'titleが被らない場合にバリデーションエラーが起きないか' do
       user = create(:user)
