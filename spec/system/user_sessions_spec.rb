592245c687f78bbd1aab674e9f4516bd0f39d1bd
河野菖太 <peprium@kounonoMacBook-Air.local>
2024-12-18 15:18:54 +0900

    test: Adding RSpec test user_sessions_spec.rb

--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,21 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe "UserSessions", type: :system do
-  # let(:user) { create(:user) }
+  let(:user) { create(:user) }
 
-  # describe "ログイン" do
-  #   context "認証情報が正しい場合" do
-  #     it "ログインが成功する" do
-  #       visit login_path
-  #       fill_in "email" with: user.email
-  #       fill_in "password" with: "password"
-  #       click_button "ログイン"
+  describe "ログイン" do
+    context "認証情報が正しい場合" do
+      it "ログインが成功する" do
+        visit root_path
+        click_link "ログイン"
+        fill_in "session-email", with: user.email
+        fill_in "session-password", with: "complex-password-complex"
+        click_button "ログイン"
 
-  #       expect(current_path).to eq home_pages_path
-  #       expect(page).to have_content "ログインしました"
-  #     end
-  #   end
-  # end
+        expect(page).to have_content("ログインしました")
+        expect(current_path).to eq tasks_path
+      end
+    end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    context "メールアドレスに誤りがある場合" do
+      it "ログインに失敗する" do
+        visit root_path
+        click_link "ログイン"
+        fill_in "session-email", with: "miss@email.com"
+        fill_in "session-password", with: "password"
+        click_button "ログイン"
+
+        expect(page).to have_content("ユーザーが存在しません")
+        expect(current_path).to eq login_path
+      end
+    end
+
+    context "パスワードに誤りがある場合" do
+      it "ログインに失敗する" do
+        visit root_path
+        click_link "ログイン"
+        fill_in "session-email", with: user.email
+        fill_in "session-password", with: "misspassword"
+        click_button "ログイン"
+
+        expect(page).to have_content("ユーザーが存在しません")
+        expect(current_path).to eq login_path
+      end
+    end
+  end
+
+  describe "ログアウト" do
+    before { login(user) }
+    it "ログアウト後、トップ画面にリダイレクトされる" do
+      click_link "ログアウト"
+
+      expect(page).to have_content("ログアウトしました")
+      expect(current_path).to eq root_path
+    end
+  end
 end
