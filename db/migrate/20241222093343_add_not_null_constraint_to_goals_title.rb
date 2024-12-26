6ecd60ae690070f190945dbf52bb29b9a3d3a6e6
河野菖太 <peprium@kounonoMacBook-Air.local>
2024-12-26 11:54:54 +0900

    delete: Delete some unused tables and models

--- db/migrate/20241222093343_add_not_null_constraint_to_goals_title.rb
+++
@@ -1,5 +0,0 @@
-class AddNotNullConstraintToGoalsTitle < ActiveRecord::Migration[8.0]
-  def change
-    change_column_null :goals, :title, false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,29 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_23_020717) do
+ActiveRecord::Schema[8.0].define(version: 2024_11_25_002914) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "goals", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "discription"
-    t.integer "user_id"
-    t.integer "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "landmarks", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "landmark_image", null: false
-    t.integer "setting_height", null: false
-    t.integer "setting_height_level", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "tasks", force: :cascade do |t|
+    t.integer "user_id", null: false
     t.string "title", null: false
     t.text "body", null: false
     t.datetime "deadline"
@@ -40,10 +23,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_23_020717) do
     t.integer "progress_status", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.bigint "goal_id", null: false
-    t.index ["goal_id"], name: "index_tasks_on_goal_id"
-    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -57,7 +36,4 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_23_020717) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
-
-  add_foreign_key "tasks", "goals"
-  add_foreign_key "tasks", "users"
 end
