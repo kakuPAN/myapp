e6c1b181d31487b97dc59f7630baaf96c7a6ece8
河野菖太 <peprium@kounonoMacBook-Air.local>
2025-02-08 18:09:46 +0900

    fix: Modify RSpec system tests to include Google OAuth2 login mock

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -273,7 +273,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :google_oauth2, Rails.application.credentials.google[:client_id], Rails.application.credentials.google[:secret_id]
+  config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :client_id), Rails.application.credentials.dig(:google, :secret_id)
   # config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_SECRET_ID']
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
